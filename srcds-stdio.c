bfd29b6e32035c30d6ddfd677ab71aafa1234e84
Michał Garapich <michal@garapich.pl>
2017-11-11 18:53:23 +0100

    Launch srcds-linux

--- srcds-stdio.c
+++ srcds-stdio.c
@@ -1,17 +1,18 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-const char* TMUX_EXEC = "/usr/bin/tmux";
-const char* SRCDS_EXEC = "./srcds_linux";
-const char* TMUX_SESSION_NAME = "srcds";
-const char* FIFO_PATH = "/tmp/srcds-stdio";
+const char *TMUX_EXEC = "/usr/bin/tmux";
+const char *SRCDS_EXEC = "./srcds_linux";
+const char *TMUX_SESSION_NAME = "srcds";
+const char *FIFO_PATH = "/tmp/srcds-stdio";
 const int CMD_SIZE = 256;
 
-static int tmux_create_session(const char* name)
+static int tmux_create_session(const char *name)
 {
     char cmd[CMD_SIZE];
     snprintf(cmd, CMD_SIZE, "%s new -d -s %s", TMUX_EXEC, name);
@@ -28,20 +29,39 @@ static int tmux_create_session(const char* name)
     return ret;
 }
 
-static int tmux_send_keys(const char* session_name, const char* keys)
+static int tmux_send_keys(const char *session_name, const char* keys)
 {
     char cmd[CMD_SIZE];
     snprintf(cmd, CMD_SIZE, "%s send-keys -t %s '%s' C-m", TMUX_EXEC, session_name, keys);
+    printf("[ %s ]\n", cmd);
 
     return system(cmd);
 }
 
-static int tmux_kill_session(const char* name)
+static int tmux_kill_session(const char *name)
 {
     char cmd[CMD_SIZE];
     snprintf(cmd, CMD_SIZE, "%s kill-session -t %s", TMUX_EXEC, name);
 
     return system(cmd);
+}
+
+/**
+ * Pass arguments to srcds_linux.
+ */
+static char* build_cmd(int argc, char **argv)
+{
+    char *cmd = malloc(CMD_SIZE);
+    snprintf(cmd, CMD_SIZE, "%s ", SRCDS_EXEC);
+
+    for (int i = 1; i < argc; ++i) {
+        strcat(cmd, argv[i]);
+        strcat(cmd, " ");
+    }
+
+    strcat(cmd, "; echo 1 > ");
+    strcat(cmd, FIFO_PATH);
+    return cmd;
 }
 
 int main(int argc, char **argv)
@@ -69,10 +89,17 @@ int main(int argc, char **argv)
         return ret;
     }
 
-    char keys[256];
-    snprintf(keys, 256, "echo dupa; sleep 2; echo 1 > %s", FIFO_PATH);
+    char *cmd = build_cmd(argc, argv);
+    printf("%s\n", cmd);
 
-    ret = tmux_send_keys(TMUX_SESSION_NAME, keys);
+    ret = tmux_send_keys(TMUX_SESSION_NAME, cmd);
+    if (ret) {
+        fprintf(stderr, "Could not send keys to session: %s", TMUX_SESSION_NAME);
+        return ret;
+    }
+
+    sleep(30);
+    ret = tmux_send_keys(TMUX_SESSION_NAME, "quit");
     if (ret) {
         fprintf(stderr, "Could not send keys to session: %s", TMUX_SESSION_NAME);
         return ret;
